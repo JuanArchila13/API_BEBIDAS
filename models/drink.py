de824be3006c43e7d99372c9dfa0dad765403150
JuanArchila13 <juanpabloarchila2003@outlook.com>
2025-04-22 15:55:55 -0500

    Added poblation script for Drink entity

--- models/drink.py
+++ models/drink.py
@@ -6,7 +6,7 @@ Base = declarative_base()
 
 class Drink(Base):
     __tablename__ = "drinks"
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String(100), unique=True, index=True)
     size = Column(String(50))
     price = Column(Float)
---
+++ poblation_scripts/POBLATE-DRINKS.sql
@@ -0,0 +1,19 @@
+INSERT INTO drinks_db.drinks (name, size, price) VALUES
+('Cerveza', '8 Oz', 6000),
+('Jugo de Naranja', '12 Oz', 4500),
+('Gaseosa', '16 Oz', 3500),
+('Té Frío', '12 Oz', 4000),
+('Agua Mineral', '20 Oz', 3000),
+('Café Americano', '10 Oz', 2500),
+('Chocolate Caliente', '10 Oz', 3200),
+('Jugo de Mango', '12 Oz', 4800),
+('Malteada de Vainilla', '16 Oz', 5200),
+('Refresco de Limón', '12 Oz', 3700),
+('Limonada Natural', '16 Oz', 3900),
+('Té Verde', '8 Oz', 3100),
+('Batido de Fresa', '14 Oz', 5000),
+('Jugo de Piña', '12 Oz', 4600),
+('Café con Leche', '10 Oz', 2800);
+
+
+SELECT * FROM drinks_db.drinks;
